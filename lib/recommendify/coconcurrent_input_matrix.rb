cfa04c83df22a2f49786e2e3bafc336ec1feac2a
Alexander Junger <alexander.junger@runtastic.com>
2017-10-12 14:39:16 +0200

    Add ccmatrix, remove unnecessary methods

--- lib/recommendify/coconcurrent_input_matrix.rb
+++ lib/recommendify/coconcurrent_input_matrix.rb
@@ -1,35 +1,17 @@
 class Recommendify::CoconcurrentInputMatrix < Recommendify::InputMatrix
 
+  include Recommendify::CCMatrix
+
   def initialize(opts={})
     check_native if opts[:native]
     super(opts)
   end
 
-  # def similarity(item1, item2)
-  #   calculate_jaccard_cached(item1, item2)
-  # end
-
   def similarities_for(item1)
     return run_native(item1) if @opts[:native]
-    # calculate_similarities(item1)
   end
 
 private
-
-  # def calculate_similarities(item1)
-  #   (all_items - [item1]).map do |item2|
-  #     [item2, similarity(item1, item2)]
-  #   end
-  # end
-
-  # def calculate_jaccard_cached(item1, item2)
-  #   val = ccmatrix[item1, item2]
-  #   val.to_f / (item_count(item1)+item_count(item2)-val).to_f
-  # end
-
-  # def calculate_jaccard(set1, set2)
-  #   (set1&set2).length.to_f / (set1 + set2).uniq.length.to_f
-  # end
 
   def run_native(item_id)
     res = %x{#{native_path} --coconcurrent "#{redis_key}" "#{item_id}" "#{redis_url}"}
